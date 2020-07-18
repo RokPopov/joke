d42f6eaa06035afc4bc0d54a0feb7fd442468809
rokpopov <rokpopov90@gmail.com>
2020-07-18 19:27:36 +0200

    The actual joke

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+ 
+const app = express();
+
+const page = `
+<html>
+<head>
+<title>Rok Popov Ledinski</title>
+</head>
+<body>
+<h1>Why was the JavaScript reality show cancelled after only one episode?</h1>
+<p>People thought it seemed scripted.</p>
+</body>
+</html>`
+
+app.get('/', (request, response) => {
+  response.send(page);
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}.`)
+})
