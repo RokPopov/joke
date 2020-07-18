d0f902db483840f99ff3acfbe898abbc48f533a3
rokpopov <rokpopov90@gmail.com>
2020-07-18 19:49:55 +0200

    Adding a port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (request, response) => {
   response.send(page);
 })
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on ${port}.`)
